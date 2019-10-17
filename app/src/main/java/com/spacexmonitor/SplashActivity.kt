d7d03e7f4019bf446805a64d45eba0f3b82cdf3e
Yevhen Radchenko <yevhen.y.radchenko@gmail.com>
2019-10-17 19:10:16 +0300

    Splash and default activitys added

---
+++ app/src/main/java/com/spacexmonitor/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.spacexmonitor
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SPLASH_DISPLAY_TIME: Long = 1700
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_DISPLAY_TIME)
+    }
+}
